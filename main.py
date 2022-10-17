5e1cbcdd53c5beb341f1c741d947bc6a1831647d
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-10-17 18:04:43 -0300

    Adiciona sanitizador e validação de url vazia

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
-print(url)
+# url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
+url = "   "
+
+# Sanitização da URL
+url = url.strip()
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
 
 # Separa base e os parâmetros
 indice_interrogacao = url.find('?')
